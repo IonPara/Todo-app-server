d6d9dc6a7e2c3106767b2e732c24c44e4be516fc
Ion Para <para_ion@hotmail.com>
2023-03-03 18:16:06 +0000

    Add cors

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-app.use(cors());
-
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
